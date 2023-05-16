836375390d9f892f8ba13451713d5430229e57e7
minami murata <minami_murata@outlook.com>
2023-05-16 23:34:24 +0900

    commit ruby_exam jyanken

--- janken.rb
+++ janken.rb
@@ -6,22 +6,22 @@ class Player
       puts "0:グー, 1:チョキ, 2:パー"
       # 変数「input_hand」にプレイヤーの入力値を代入します。
       # ヒント：getsメソッドについて調べてみましょう。
-      input_hand = gets.to_i
+      input_hand = gets.chomp
       # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
       while true 
         # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
         # ヒント：include?メソッドについて調べてみましょう。
-        if input_hand == 0 || input_hand == 1 || input_hand == 2  
+        if input_hand == "0" || input_hand== "1" || input_hand == "2"  
           # 「input_hand」をそのまま返す。
           # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
-          return input_hand
+          return input_hand.to_i
         # else それ以外の場合
         else
           # プレイヤーに「0〜2」を入力させる文章を表示させる。
           puts "0〜2の数字を入力してください。"
           puts "0:グー, 1:チョキ, 2:パー"
           # 変数「input_hand」にプレイヤーの入力値を代入します。
-          input_hand = gets.to_i
+          input_hand = gets.chomp
         # end if文のend
         end
       end
